92ed6219e0e180b0a245ec4a6fea1f768e4aaabc
nico <nicolasalfonsoferreira@gmail.com>
2024-12-03 18:22:59 -0300

    finished the add/delete functionality with page re-rendering

--- reading/views.py
+++ reading/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 
 from .models import Book
@@ -9,3 +9,16 @@ class ReadingListView(ListView):
     template_name = "reading/reading_list.html"
     context_object_name = "book_list"
 
+    def post(self, request, *args, **kwargs):
+        # Handling form submission (basic way, without using ModelForm)
+        book_title = request.POST.get("book-title")
+        book_author = request.POST.get("book-author")
+
+        if book_title and book_author: # if a new book is being added to the list
+            new_book = Book(title=book_title, author=book_author)
+            new_book.save()
+        else: # if a book is being removed from the list
+            Book.objects.get(pk=request.POST.get("book-pk")).delete()
+
+        # Redirecting after form submission to prevent resubmission on page refresh
+        return redirect('reading_list')  # Use the name of your URL pattern here
