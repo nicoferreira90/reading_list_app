79b38c90ff91001394af94357e5bd58a745ba304
nico <nicolasalfonsoferreira@gmail.com>
2024-12-04 16:34:03 -0300

    implement simple create and delete AJAX functionality with HTMX

--- reading/urls.py
+++ reading/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import ReadingListView
+from .views import ReadingListView, add_book, delete_book
 
 urlpatterns = [
-    path("", ReadingListView.as_view(), name="reading_list"),
+    path("", ReadingListView.as_view(), name="reading_page"),
 ]
+
+htmx_urlpatterns = [
+    path("add_book/", add_book, name="add_book"),
+    path("delete_book/<int:pk>/", delete_book, name="delete_book")
+]
+
+urlpatterns += urlpatterns+htmx_urlpatterns
--- reading/views.py
+++ reading/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
+from django.views.decorators.http import require_http_methods
 
 from .models import Book
 
 
 class ReadingListView(ListView):
     model = Book
-    template_name = "reading/reading_list.html"
+    template_name = "reading/reading_page.html"
     context_object_name = "book_list"
 
-    def post(self, request, *args, **kwargs):
-        # Handling form submission (basic way, without using ModelForm)
-        book_title = request.POST.get("book-title")
-        book_author = request.POST.get("book-author")
+def add_book(request):
+    title = request.POST.get('book-title')
+    author = request.POST.get('book-author')
+    Book.objects.create(title=title, author=author)
 
-        if book_title and book_author: # if a new book is being added to the list
-            new_book = Book(title=book_title, author=book_author)
-            new_book.save()
-        else: # if a book is being removed from the list
-            Book.objects.get(pk=request.POST.get("book-pk")).delete()
+    book_list = Book.objects.all()
+    return render(request, 'reading/book_list.html', {'book_list': book_list})
 
-        # Redirecting after form submission to prevent resubmission on page refresh
-        return redirect('reading_list')  # Use the name of your URL pattern here
+@require_http_methods(['DELETE'])
+def delete_book(request, pk):
+    Book.objects.filter(pk=pk).delete()
+    book_list = Book.objects.all()
+    return render(request, 'reading/book_list.html', {'book_list': book_list})
+
